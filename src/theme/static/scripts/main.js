18c13fa197005373b15a0cd27e52ebb246229215
Ryan Phillips <ryanmphill@gmail.com>
2025-04-12 00:32:04 -0500

    Minor adjustment to scroll container height

--- src/theme/static/scripts/main.js
+++ src/theme/static/scripts/main.js
@@ -1,3 +1,16 @@
 import { addDisclosureFunctionalityToSubmenus } from "./navbar.js";
 
 addDisclosureFunctionalityToSubmenus(".navbar", "navbar__links");
+
+const scrollContainer = document.querySelector(".parallax");
+
+let ticking = false;
+scrollContainer.addEventListener("scroll", function () {
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      window.scrollTo(0, scrollContainer.scrollTop);
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
