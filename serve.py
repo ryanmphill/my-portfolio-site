2fabebc4f1ebfadcb6b4087ba6ac8b38bcb44b5a
Ryan Phillips <ryanmphill@gmail.com>
2025-06-16 23:30:55 -0500

    Added esbuild to bundle and minify JavaScript as part of the build step

--- serve.py
+++ serve.py
@@ -5,10 +5,13 @@ the web server
 """
 
 import os
+import subprocess
 from shodo_ssg import build_static_site, start_server
 
 if __name__ == "__main__":
     # Set the ROOT_PATH variable to the directory of this file
     root_path = os.path.dirname(os.path.abspath(__file__))
+    # Bundle and minify the JavaScript files
+    subprocess.run(["npm", "run", "dev"], check=True)
     build_static_site(root_path)
     start_server(root_path)
