5acd012e8c7b964efbb61ad5b4ec4687e77ed48a
Ryan Phillips <ryanmphill@gmail.com>
2026-01-24 12:50:08 -0600

    add simple css minification to build step

--- serve.py
+++ serve.py
@@ -5,8 +5,31 @@ the web server
 """
 
 import os
+import re
 import subprocess
 from shodo_ssg import build_static_site, start_server
+
+
+def minify_css(css_file_path):
+    """Minify a CSS file located at css_file_path and write the minified version"""
+    with open(css_file_path, "r", encoding="utf-8") as f:
+        css = f.readlines()
+
+    minified_css = "".join(line.strip() for line in css)
+
+    # remove comments
+    minified_css = re.sub(r"/\*[\s\S]*?\*/", "", minified_css)
+
+    # remove quotes from url()
+    minified_css = re.sub(r'url\((["\'])([^)]*)\1\)', r"url(\2)", minified_css)
+
+    # collapse spaces
+    minified_css = re.sub(r"\s+", " ", minified_css)
+
+    # write out the minified css
+    with open(css_file_path.replace(".css", ".css"), "w", encoding="utf-8") as f:
+        f.write(minified_css)
+
 
 if __name__ == "__main__":
     # Set the ROOT_PATH variable to the directory of this file
@@ -14,4 +37,5 @@ if __name__ == "__main__":
     # Bundle and minify the JavaScript files
     subprocess.run(["npm", "run", "dev"], check=True)
     build_static_site(root_path)
+    minify_css(os.path.join(root_path, "dist", "static", "styles", "main.css"))
     start_server(root_path)
--- site_builder.py
+++ site_builder.py
@@ -4,7 +4,30 @@ templates and all static assets
 """
 
 import os
+import re
 from shodo_ssg import build_static_site
+
+
+def minify_css(css_file_path):
+    """Minify a CSS file located at css_file_path and write the minified version"""
+    with open(css_file_path, "r", encoding="utf-8") as f:
+        css = f.readlines()
+
+    minified_css = "".join(line.strip() for line in css)
+
+    # remove comments
+    minified_css = re.sub(r"/\*[\s\S]*?\*/", "", minified_css)
+
+    # remove quotes from url()
+    minified_css = re.sub(r'url\((["\'])([^)]*)\1\)', r"url(\2)", minified_css)
+
+    # collapse spaces
+    minified_css = re.sub(r"\s+", " ", minified_css)
+
+    # write out the minified css
+    with open(css_file_path.replace(".css", ".css"), "w", encoding="utf-8") as f:
+        f.write(minified_css)
+
 
 if __name__ == "__main__":
     # Set the ROOT_PATH variable to the directory of this file
@@ -12,3 +35,4 @@ if __name__ == "__main__":
     # Bundle and minify the JavaScript files
     os.system("npm run build")
     build_static_site(root_path)
+    minify_css(os.path.join(root_path, "dist", "static", "styles", "main.css"))
